8317cec3b83255527e117de9e762bb00c41eb2fd
Nina Adora <nirinakaes@gmai.com>
2026-02-02 22:28:55 +0300

    creating  a variable named maleNames

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 const maleNames = 
-[Kwasi,Kwadwo,	Kwabena,	Kwaku,]
+[Kwasi,Kwadwo,	Kwabena ,Kwaku, Yaw,Kofi,Kwame]
+
+const femaleNames=
+[ Akosua,Adwoa,Abenaa,Akua,Yaa,	Afua,Ama]
+
+Validate date
+ if(date <=0 || >31){
+    alert(Enter a valid date)
+ }
+
+
+validate month
+if(month <=0 || >12){
+    alert(Enter a valid month)
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 9
+
