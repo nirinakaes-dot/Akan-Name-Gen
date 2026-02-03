50e48310ac8676f0e939b9bd4448d33dd4b91a80
Nina Adora <nirinakaes@gmai.com>
2026-02-03 09:40:30 +0300

    tagging my js

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
+//Give variable to male names
 const maleNames = 
 [Kwasi,Kwadwo,	Kwabena ,Kwaku, Yaw,Kofi,Kwame]
 
+//Give a variable to all Female Names
 const femaleNames=
 [ Akosua,Adwoa,Abenaa,Akua,Yaa,	Afua,Ama]
 
-Validate date
+//Validate date
  if(date <=0 || >31){
     alert(Enter a valid date)
  }
 
 
-Validate month
+//Validate month
 if(month <=0 || >12){
     alert(Enter a valid month)
 }
 
-
+//Assign variables to Days
 let DD= parseInt(document.getElementById("Birth Day"))
 
-
+//Assign Variables to Months
 let MM=parseInt(document.getElementById("Birth Month"))
 
-
+//Assign Varabled to Years
 let YEAR=parseInt(document.getElementById("Birth Year"))
 
+//Split the Year into two
+let CC= parseInt(YEAR. slice
+    (0,2)
+)
 
-let CC= parseInt()
-
-let YY=parseInt()
+let YY=parseInt(YEAR. slice(0,4)
+ )
 
 
 
